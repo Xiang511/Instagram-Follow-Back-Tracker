baac0942d832a0a442c23c3158bec8f76e8278ac
Xiang511 <toby91511@gmail.com>
2024-07-30 23:01:05 +0800

    feat: Website information layout

--- threads.js
+++ threads.js
@@ -128,3 +128,30 @@ function main() {
         alert('Please upload information first (follower and following)');
     }
 }
+
+
+
+//即時查詢總人數
+const firstSpan = document.querySelector('.sitestatesJs');
+//取得字串
+const text = firstSpan.textContent;
+//取得數字
+const Real_Time_Number = text.substring(text.length - 4);
+
+
+//今日訪客
+const spans = document.querySelectorAll(".sitestatesJs");
+const secondSpan = spans[1];
+const secondSpan_txt = secondSpan.textContent;
+const secondSpan_result = secondSpan_txt.substring(text.length - 4);
+
+//線上人數
+const online = document.querySelectorAll(".sitestatesJs");
+const onlineSpan = online[2];
+const onlineSpan_txt = onlineSpan.textContent;
+const onlineSpan_result = onlineSpan_txt.substring(text.length - 4);
+
+document.getElementById("Total").innerHTML = "累計訪客:"+"&ensp;" +Real_Time_Number;
+// document.getElementById("month").innerHTML = "本月訪客:"+ month;
+document.getElementById("Today").innerHTML = "今日訪客:" +"&ensp;" +secondSpan_result;
+document.getElementById("online").innerHTML = "線上人數:" +"&ensp;" +onlineSpan_result;
